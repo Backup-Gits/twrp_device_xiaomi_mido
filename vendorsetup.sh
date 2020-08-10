1459cc0dd682be5b060ac6ebc2b7715073ed6b3b
Herobuxx <herobuxx@gmail.com>
2020-08-11 02:30:47 +0700

    mido: Add vendorsetup.sh and set to R11
    Signed-off-by: Herobuxx <herobuxx@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+export FOX_R11=1
+export FOX_ADVANCED_SECURITY=1
+export OF_USE_TWRP_SAR_DETECT=1
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
